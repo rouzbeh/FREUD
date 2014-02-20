44a30408cb220514a8bded5f5f62e0ce6392e143
Ali Neishabouri <ali@neishabouri.net>
2014-02-20 13:33:01 +0000

    Add file containing settings

---
+++ require.php
@@ -0,0 +1,46 @@
+<?php
+//Required Variables for Freud (All Variables Are REQUIRED)
+
+// General Information
+// The title of the website.
+$FREUD_title = "FREUD";
+$FREUD_subtitle = "Participant management system;
+
+// Welcome Message -  This message is displayed on the home page.
+$welcome_message = "Welcome!";
+
+// the email address that server generated emails will come from:
+$server_email_address = "";
+
+// Email Domain - The domain all users must have to register ie: if 
+// your college's emails are bob@union.edu you would put "union.edu"
+$email_domain = "";
+
+// Default Password - This is the password that all accounts are reset to.
+$the_default_password = "123456";
+
+// DATABASE
+// the location of your MYSQL/Mariadb Server
+$servername="localhost";
+
+// The MYSQL Username
+$username="";
+
+// The MYSQL Password
+$password="";
+
+// The database name of the MYSQL
+$databasename="freuddb";
+
+// The base url where Freud is located. . ie "http://freud.union.edu/"
+// ****IMPORTANT**** this must end with a /
+$internetLocation="";
+
+// Sending emails
+$SMTPserver = "";
+$SMTPPort = "";
+$SMTPAuth = true;
+$SMTPUsername = '';
+$SMTPPassword = '';
+
+?>
